c5efbbe22e53c547e1cd9c103d62ff5eb0bcc7e8
EagleByte <28986398+TheEagleByte@users.noreply.github.com>
2023-05-01 16:58:58 -0400

    Put in tooltips, clean stuff up

--- web/src/components/RtlHeatmap.tsx
+++ web/src/components/RtlHeatmap.tsx
@@ -1,45 +1,57 @@
-import { Group } from '@visx/group';
-import { scaleLinear, scaleTime } from '@visx/scale';
-import { AxisTop, AxisLeft, } from '@visx/axis';
+import { useCallback, useMemo } from 'react';
+import { RtlData, RtlFrequency, } from '@/query/rtl';
 import { format } from 'd3-format';
 import { timeFormat } from 'd3-time-format';
-import { interpolateViridis } from 'd3-scale-chromatic';
-import { HeatmapRect } from '@visx/heatmap';
-import { RtlData, RtlDataRow } from '@/query/rtl';
 import { Typography } from '@mui/material';
+import {
+    useTooltip,
+    useTooltipInPortal,
+    defaultStyles,
+} from '@visx/tooltip';
+import { scaleLinear, scaleTime } from '@visx/scale';
+import RtlHeatmapChart from './RtlHeatmapChart';
+
+
+const formatTime = (value: any) => timeFormat("%I:%M %p")(new Date(value));
+const formatFrequency = (value: any) => format(".3s")(value) + "Hz";
 
 const background = '#28272c';
-const tickLabelColor = '#fff';
+const margin = { top: 60, left: 100, right: 35, bottom: 20 };
 
-const tickLabelProps = {
-    fill: tickLabelColor,
-    fontSize: 12,
-    fontFamily: 'sans-serif',
-    textAnchor: 'middle',
+export type TooltipProps = {
+    width: number;
+    height: number;
+    showControls?: boolean;
+};
+
+type TooltipData = {
+    time: string;
+    freq: string;
+    frequency: RtlFrequency | null | undefined;
+};
+
+const defaultData = {
+    time: '',
+    freq: '',
+    frequency: null
 }
+
+const positionIndicatorSize = 8;
 
 export type HeatmapProps = {
     data: RtlData;
     width: number;
     height: number;
-    margin?: { top: number; right: number; bottom: number; left: number };
-    events?: boolean;
 };
 
-const margin = { top: 60, left: 100, right: 35, bottom: 20 };
-
-const bins = (d: RtlDataRow) => d.bins;
-
-function max<Datum>(data: Datum[], value: (d: Datum) => number): number {
-    return Math.max(...data.map(value));
-}
-
-function min<Datum>(data: Datum[], value: (d: Datum) => number): number {
-    return Math.min(...data.map(value));
-}
-
-const formatTime = (value: any) => timeFormat("%I:%M %p")(new Date(value));
-const formatFrequency = (value: any) => format(".3s")(value) + "Hz";
+const tooltipStyles = {
+    ...defaultStyles,
+    backgroundColor: background,
+    color: 'white',
+    width: 160,
+    height: 62,
+    padding: 12,
+};
 
 function RtlHeatmap({
     data,
@@ -57,95 +69,145 @@ function RtlHeatmap({
     const yMax = height - margin.bottom - margin.top;
 
     // scales
-    const xScale = scaleLinear({
+    const xScale = useMemo(() => scaleLinear({
         domain: stats.freqRange,
         range: [0, xMax]
-    });
-    const yScale = scaleTime({
+    }), [stats.freqRange, xMax]);
+
+    const yScale = useMemo(() => scaleTime({
         domain: [new Date(stats.timeRange[0]), new Date(stats.timeRange[1])],
         range: [yMax, 0],
         reverse: true
-    });
-    const colorScale = scaleLinear({
+    }), [stats.timeRange, yMax]);
+
+    const colorScale = useMemo(() => scaleLinear({
         range: [0, 1],
         domain: stats.dbRange
+    }), [stats.dbRange]);
+
+    const { containerRef, containerBounds, TooltipInPortal } = useTooltipInPortal({
+        scroll: true,
+        detectBounds: true,
     });
+
+    const {
+        showTooltip,
+        hideTooltip,
+        tooltipOpen,
+        tooltipData,
+        tooltipLeft = 0,
+        tooltipTop = 0,
+    } = useTooltip<TooltipData>({
+        tooltipOpen: false,
+        tooltipLeft: width / 3,
+        tooltipTop: height / 3,
+        tooltipData: defaultData,
+    });
+
+    const handlePointerMove = useCallback(
+        (event: React.PointerEvent<HTMLDivElement>) => {
+            const actualBounds = {
+                left: containerBounds.left + margin.left,
+                top: containerBounds.top + margin.top,
+                width: containerBounds.width - margin.left - margin.right,
+                height: containerBounds.height - margin.top - margin.bottom
+            };
+
+            // If the event is outside the bounds of the chart, hide the tooltip
+            if (event.clientX < actualBounds.left || event.clientX > actualBounds.left + actualBounds.width ||
+                event.clientY < actualBounds.top || event.clientY > actualBounds.top + actualBounds.height) {
+                hideTooltip();
+                return;
+            }
+
+            // coordinates should be relative to the container in which Tooltip is rendered
+            const containerX = ('clientX' in event ? event.clientX : 0) - containerBounds.left;
+            const containerY = ('clientY' in event ? event.clientY : 0) - containerBounds.top;
+
+            const x0 = xScale.invert(containerX - margin.left);
+            const y0 = yScale.invert(containerY - margin.top);
+
+            // Find a frequency that's within the freqStart and freqStop points of x0
+            const frequency = frequencies.find(f => f.freqStart <= x0 && f.freqStop >= x0);
+
+            showTooltip({
+                tooltipLeft: containerX,
+                tooltipTop: containerY,
+                tooltipData: {
+                    time: formatTime(y0),
+                    freq: formatFrequency(x0),
+                    frequency
+                }
+            });
+        },
+        [containerBounds.left, containerBounds.top, containerBounds.width, containerBounds.height, xScale, yScale, frequencies, showTooltip, hideTooltip],
+    );
 
     if (width < 10) return <Typography variant="h6">Too small</Typography>;
     return (
-        <svg width={width} height={height}>
-            <rect x={0} y={0} width={width} height={height} rx={14} fill={background} />
-            <Group top={margin.top} left={margin.left}>
-                <HeatmapRect
-                    data={rows}
-                    xScale={xScale}
-                    yScale={yScale}
-                    gap={0}
-                >
-                    {(heatmap) => heatmap.map((heatmapBins, heatIndex) => {
-                        return heatmapBins.map((bin, binIndex) => {
-                            const x = xScale(rows[heatIndex].bins[binIndex].bin);
-                            const y = yScale(new Date(rows[heatIndex].bin));
-                            const h = (heatIndex != rows.length - 1 && binIndex < rows[heatIndex + 1].bins.length)
-                                ? yScale(new Date(rows[heatIndex + 1].bin)) - yScale(new Date(rows[heatIndex].bin))
-                                : yScale(new Date(rows[heatIndex].bin)) - yScale(new Date(rows[heatIndex - 1].bin));
-                            const w = xScale(rows[heatIndex].bins[binIndex].bin + stats.freqStep) - xScale(rows[heatIndex].bins[binIndex].bin);
-
-                            return (
-                                <rect
-                                    key={`heatmap-rect-${bin.row}-${bin.column}`}
-                                    width={w}
-                                    height={h}
-                                    x={x}
-                                    y={y}
-                                    fill={interpolateViridis(colorScale(bin.count!)) ?? '#000'}
-                                    fillOpacity={bin.opacity}
-                                />
-                            )
-                        })
-                    })}
-                </HeatmapRect>
-                <AxisLeft
-                    scale={yScale}
-                    tickFormat={formatTime}
-                    label="Time"
-                    labelProps={{
-                        fill: "#fff",
-                        fontSize: 12,
-                        textAnchor: 'middle'
-                    }}
-                    labelOffset={70}
-                    tickStroke={"#fff"}
-                    tickLabelProps={{
-                        fill: "#fff",
-                        fontSize: 12,
-                        textAnchor: 'end'
-                    }}
-                    stroke="#fff"
-                />
-                <AxisTop
-                    scale={xScale}
-                    top={0}
-                    left={0}
-                    label="Frequency"
-                    labelProps={{
-                        fill: "#fff",
-                        fontSize: 12,
-                        textAnchor: 'middle'
-                    }}
-                    labelOffset={20}
-                    tickFormat={formatFrequency}
-                    tickStroke={"#fff"}
-                    numTicks={16}
-                    tickLabelProps={{
-                        fill: "#fff",
-                        fontSize: 12,
-                        textAnchor: 'middle'
-                    }}
-                    stroke="#fff"
-                />
-            </Group>
-        </svg>
+        <div ref={containerRef} style={{ width, height }} onPointerMove={handlePointerMove} className="tooltip">
+            <RtlHeatmapChart
+                data={data}
+                width={width}
+                height={height}
+                margin={margin}
+                formatTime={formatTime}
+                formatFrequency={formatFrequency}
+                xScale={xScale}
+                yScale={yScale}
+                colorScale={colorScale}
+            />
+            {tooltipOpen && (
+                <>
+                    <div
+                        className="position-indicator"
+                        style={{ transform: `translate(${tooltipLeft - positionIndicatorSize / 2}px, ${tooltipTop - positionIndicatorSize / 2}px)` }}
+                    />
+                    <div
+                        className="crosshair horizontal"
+                        style={{ transform: `translateY(${tooltipTop}px)` }}
+                    />
+                    <div
+                        className="crosshair vertical"
+                        style={{ transform: `translateX(${tooltipLeft}px)` }}
+                    />
+                    {/* {tooltipData?.frequency && (
+                        <>
+                            <div
+                                className="freqBand-left"
+                                style={{ transform: `translateX(${xScale(tooltipData.frequency.freqStart) + margin.left}px)` }}
+                            />
+                            <div
+                                className="freqBand-right"
+                                style={{ transform: `translateX(${xScale(tooltipData.frequency.freqStop) + margin.left}px)` }}
+                            />
+                        </>
+                    )} */}
+                    <TooltipInPortal
+                        key={Math.random()} // needed for bounds to update correctly
+                        left={tooltipLeft}
+                        top={tooltipTop}
+                        style={tooltipStyles}
+                    >
+                        {/* {tooltipData?.frequency && (
+                            <Typography variant="body2" sx={{ fontWeight: 'bold' }}>
+                                {tooltipData.frequency.description}
+                            </Typography>
+                        )} */}
+                        {tooltipData && (
+                            <>
+                                <Typography variant="body2">
+                                    <strong>Time: </strong>{tooltipData.time}
+                                </Typography>
+                                <Typography variant="body2">
+                                    <strong>Frequency: </strong>{tooltipData.freq}
+                                </Typography>
+                            </>
+                        )}
+                    </TooltipInPortal>
+                </>
+            )}
+        </div>
     );
 }
 
---
+++ web/src/components/RtlHeatmapChart.tsx
@@ -0,0 +1,119 @@
+import { memo } from 'react';
+import { Group } from '@visx/group';
+import { ScaleLinear, ScaleTime } from 'd3-scale';
+import { AxisTop, AxisLeft, } from '@visx/axis';
+import { interpolateViridis } from 'd3-scale-chromatic';
+import { HeatmapRect } from '@visx/heatmap';
+import { RtlData } from '@/query/rtl';
+import { Typography } from '@mui/material';
+
+const background = '#28272c';
+
+export type HeatmapChartProps = {
+    data: RtlData;
+    width: number;
+    height: number;
+    margin: { top: number; right: number; bottom: number; left: number };
+    formatTime: (value: any) => string;
+    formatFrequency: (value: any) => string;
+    xScale: ScaleLinear<number, number, never>;
+    yScale: ScaleTime<number, number, never>;
+    colorScale: ScaleLinear<number, number, never>;
+};
+
+function RtlHeatmapChart({
+    data,
+    width,
+    height,
+    margin,
+    formatTime,
+    formatFrequency,
+    xScale,
+    yScale,
+    colorScale
+}: HeatmapChartProps) {
+    const {
+        frequencies,
+        data: rows,
+        stats
+    } = data;
+
+    return (
+        <svg width={width} height={height}>
+            <rect x={0} y={0} width={width} height={height} rx={14} fill={background} />
+            <Group top={margin.top} left={margin.left}>
+                <HeatmapRect
+                    data={rows}
+                    xScale={xScale}
+                    yScale={yScale}
+                    gap={0}
+                >
+                    {(heatmap) => heatmap.map((heatmapBins, heatIndex) => {
+                        return heatmapBins.map((bin, binIndex) => {
+                            const x = xScale(rows[heatIndex].bins[binIndex].bin);
+                            const y = yScale(new Date(rows[heatIndex].bin));
+                            const h = (heatIndex != rows.length - 1 && binIndex < rows[heatIndex + 1].bins.length)
+                                ? yScale(new Date(rows[heatIndex + 1].bin)) - yScale(new Date(rows[heatIndex].bin))
+                                : yScale(new Date(rows[heatIndex].bin)) - yScale(new Date(rows[heatIndex - 1].bin));
+                            const w = xScale(rows[heatIndex].bins[binIndex].bin + stats.freqStep) - xScale(rows[heatIndex].bins[binIndex].bin);
+
+                            return (
+                                <rect
+                                    key={`heatmap-rect-${bin.row}-${bin.column}`}
+                                    width={w}
+                                    height={h}
+                                    x={x}
+                                    y={y}
+                                    fill={interpolateViridis(colorScale(bin.count!)) ?? '#000'}
+                                    fillOpacity={bin.opacity}
+                                />
+                            )
+                        })
+                    })}
+                </HeatmapRect>
+                <AxisLeft
+                    scale={yScale}
+                    tickFormat={formatTime}
+                    label="Time"
+                    labelProps={{
+                        fill: "#fff",
+                        fontSize: 12,
+                        textAnchor: 'middle'
+                    }}
+                    labelOffset={70}
+                    tickStroke={"#fff"}
+                    tickLabelProps={{
+                        fill: "#fff",
+                        fontSize: 12,
+                        textAnchor: 'end'
+                    }}
+                    stroke="#fff"
+                />
+                <AxisTop
+                    scale={xScale}
+                    top={0}
+                    left={0}
+                    label="Frequency"
+                    labelProps={{
+                        fill: "#fff",
+                        fontSize: 12,
+                        textAnchor: 'middle'
+                    }}
+                    labelOffset={20}
+                    tickFormat={formatFrequency}
+                    tickStroke={"#fff"}
+                    numTicks={16}
+                    tickLabelProps={{
+                        fill: "#fff",
+                        fontSize: 12,
+                        textAnchor: 'middle'
+                    }}
+                    stroke="#fff"
+                />
+            </Group>
+        </svg>
+    );
+}
+
+export default memo(RtlHeatmapChart);
+
